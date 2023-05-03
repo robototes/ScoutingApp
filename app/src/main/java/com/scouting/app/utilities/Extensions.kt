b7d0b6061912239dc263fdc4945eb263fbfbd7c8
Joseph Eng <s-engjo@bsd405.org>
2023-05-02 20:33:36 -0700

    Fix longPressEffect warnings

--- app/src/main/java/com/scouting/app/utilities/Extensions.kt
+++ app/src/main/java/com/scouting/app/utilities/Extensions.kt
@@ -2,9 +2,7 @@ package com.scouting.app.utilities
 
 import android.content.Context
 import androidx.activity.ComponentActivity
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.zIndex
 import androidx.lifecycle.ViewModel
@@ -15,9 +13,8 @@ import androidx.navigation.navOptions
 fun <T : ViewModel> Context.getViewModel(type: Class<T>): T =
     ViewModelProvider(this as ComponentActivity)[type]
 
-@Composable
-fun Modifier.longPressEffect(offset: Float?): Modifier = this.composed {
-    Modifier
+fun Modifier.longPressEffect(offset: Float?): Modifier =
+    this
         .zIndex(offset?.let { 1f } ?: 0f)
         .graphicsLayer {
             with(offset ?: 0f) { translationY = this }
@@ -26,7 +23,6 @@ fun Modifier.longPressEffect(offset: Float?): Modifier = this.composed {
                 scaleY = 1.02F
             }
         }
-}
 
 fun NavController.returnTo(route: String) = navigate(route, navOptions { popUpTo(route) { inclusive = true } })
 
