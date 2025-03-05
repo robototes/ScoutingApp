14e96d9f7e1f5312a84360ffd3ff0d81aff51e63
Chase <chasing107@gmail.com>
2025-03-04 21:18:23 -0800

    Update ScoutingView.kt
    gen save

--- app/src/main/java/com/scouting/app/view/scouting/ScoutingView.kt
+++ app/src/main/java/com/scouting/app/view/scouting/ScoutingView.kt
@@ -384,7 +384,7 @@ fun ScoutingTemplateLoadView(list: SnapshotStateList<TemplateItem>) {
                         buttonLabelOne = item.text2.toString(),
                         buttonLabelTwo = item.text3.toString(),
                         buttonLabelThree = item.text4.toString(),
-                        buttonLabelFour = item.text5.toString(), //need to resolve reference
+                        buttonLabelFour = item.text5.toString(),
                         onValueChange = {
                             item.itemValueInt!!.value = it
                         },
